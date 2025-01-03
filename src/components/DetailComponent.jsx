8d2389f78d25d2dbed66894eedbce6b9d8761e6d
PassonoTW <82664528+passontw@users.noreply.github.com>
2025-01-03 16:16:20 +0800

    feat: 建立 Detail Component

---
+++ src/components/DetailComponent.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  faAngleLeft,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import {StyledFontAwesomeIcon} from '../components/CustomStyledComponents';
+import QRCode from 'react-qr-code';
+import copyIcon from '../assets/icons/copy.png';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const TopRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const BottomRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  margin-right: 10px;
+  font-size: 1rem;
+`;
+
+const IconContainer = styled.div`
+  margin-left: auto;
+`;
+
+const StyledImg = styled.img`
+  width: 20px;
+  height: 20px;
+  filter: brightness(0) invert(1);
+`;
+const CopyIconContainer = styled.div`
+  background-color: #0c8bef;
+  border-radius: 4px;
+  margin-right: 4px;
+  padding: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 5px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  flex-grow: 1;
+`;
+
+const DetailComponent = () => {
+  return (
+    <Container>
+      <Header>
+        <TopRow>
+          <StyledFontAwesomeIcon icon={faAngleLeft} />
+          <Text>Back to Class List</Text>
+          <IconContainer>
+            <StyledFontAwesomeIcon icon={faTimes} />
+          </IconContainer>
+        </TopRow>
+        <BottomRow>
+          <Text>Join 302 Science</Text>
+        </BottomRow>
+        <BottomRow>
+          <Text>ID: X58E9647</Text>
+          <CopyIconContainer>
+            <StyledImg src={copyIcon} alt="Copy" />
+          </CopyIconContainer>
+          <Text>Link</Text>
+          <CopyIconContainer>
+            <StyledImg src={copyIcon} alt="Copy" />
+          </CopyIconContainer>
+        </BottomRow>
+      </Header>
+      <Content>
+        <QRCode value="https://www.classswift.viewsonic.io/" />
+      </Content>
+    </Container>
+  );
+};
+
+export default DetailComponent;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,52 +1,21 @@
-import styled from 'styled-components';
-import Tabs from '../components/Tabs';
-import { faAngleLeft, faUser, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { StyledFontAwesomeIcon } from '../components/CustomStyledComponents';
+import React from 'react';
+import MainComponent from '../components/MainComponent';
+import DetailComponent from '../components/DetailComponent';
 
-const Header = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-`;
-
-const TopRow = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-`;
-
-const BottomRow = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Text = styled.span`
-  margin-right: 10px;
-  font-size: 1rem;
-`;
-
-const IconContainer = styled.div`
-  margin-left: auto;
-`;
+const showMain = false;
 
 const HomeScreen = () => {
+  if (showMain) {
+    return (
+      <div>
+        <MainComponent />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Header>
-        <TopRow>
-          <StyledFontAwesomeIcon icon={faAngleLeft} />
-          <Text>Back to Class List</Text>
-          <IconContainer>
-            <StyledFontAwesomeIcon icon={faTimes} />
-          </IconContainer>
-        </TopRow>
-        <BottomRow>
-          <Text>302 Science</Text>
-          <StyledFontAwesomeIcon icon={faUser} />
-          <Text>16/30</Text>
-        </BottomRow>
-      </Header>
-      <Tabs />
+      <DetailComponent />
     </div>
   );
 };
