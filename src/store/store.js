32dabbb91474f53d54818ce5bf336bbc46a5b206
PassonoTW <82664528+passontw@users.noreply.github.com>
2025-01-03 16:47:29 +0800

    feat: 增加 redux-persist package 與設定

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,46 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'; 
 import counterReducer from './counterSlice';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import { loggerMiddleware } from '../middlewares/loggerMiddleware';
 import { apiMiddleware } from '../middlewares/apiMiddleware';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+const persistConfig = {
+  key: '!QAZ@WSX#EDC',
+  storage,
+  whitelist: ['counter'],
+  blacklist: [],
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    const defaultMiddlewares = getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['persist/PERSIST'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: [],
+        // Ignore these paths in the state
+        ignoredPaths: [],
+      },
+    });
+    return [...defaultMiddlewares, loggerMiddleware, apiMiddleware];
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(loggerMiddleware)
-      .concat(apiMiddleware),
   devTools: {
     name: 'Class Student App',
     trace: true,
     traceLimit: 25,
   },
 });
+
+const persistor = persistStore(store);
+
+export default persistor;
